ec41fcf6a1d3e8a3e92806661348fd2487c2e04b
yangaveryx <yang.averyx@gmail.com>
2026-06-17 14:08:46 -0400

    deleted old styles page

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./global.css";
-// import "./styles.css";
 
 const root = document.getElementById("root")!;
 createRoot(root).render(<App />);
