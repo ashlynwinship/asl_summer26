c98246a356ecaffb76c3237ff634bd8bfc571dd4
J Zhu <jzhu@dhcp-vl2041-19284.eduroam.cornell.edu>
2026-06-22 10:29:03 -0400

    api files and docker changes

---
+++ server/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
+
+@app.post("/pose-json")
+async def pose_coordinates_json(item: Item):
+    
